76431705cd8334151cb7590af42c993db5414338
Rafael Reis <rafaelreis@gurgelmix.com.br>
2025-02-19 02:02:13 -0300

    pagina de ranking de convites e alguns ajustes

--- src/app/components/icon-button.tsx
+++ src/app/components/icon-button.tsx
@@ -1,12 +1,20 @@
 import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface IconButtonProps extends ComponentProps<'button'> {}
 
-export function IconButton({ disabled = false, ...props }: IconButtonProps) {
+export function IconButton({
+    disabled = false,
+    className,
+    ...props
+}: IconButtonProps) {
     return (
         <button
+            className={twMerge(
+                `p-1.5 bg-gray-500 text-blue rounded-md ${!disabled ? 'cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300' : 'cursor-not-allowed'}`,
+                className
+            )}
             {...props}
-            className={`p-1.5 bg-gray-500 text-blue rounded-md ${!disabled ? 'cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300' : 'cursor-not-allowed'}`}
         />
     )
 }
